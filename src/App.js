43c74f04d19e1562fc52050cb412b47595a958aa
XavierGM23 <64241384+XavierGM23@users.noreply.github.com>
2021-03-30 11:59:05 -0500

    updating shop and collection-item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
-import HomePage from '../src/pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx'
 
 import './App.scss';
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-}
+// const HatsPage = (props) => {
+//   console.log(props);
+//   return (
+//     <div>
+//       <h1>Hats Page</h1>
+//     </div>
+//   );
+// }
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <Switch> 
           <Route exact path="/" component={HomePage}/>
-          <Route exact path="/hats" component={HatsPage}/>
+          <Route path="/shop" component={ShopPage}/>
         </Switch>  
       </div>
     );
