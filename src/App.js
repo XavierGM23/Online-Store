781049bae7b48ff437bdd1d042e9ff2d174b3bd6
XavierGM23 <64241384+XavierGM23@users.noreply.github.com>
2021-03-30 12:14:44 -0500

    Adding Header component and multipage Links into it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component.jsx';
-import ShopPage from './pages/shop/shop.component.jsx'
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 import './App.scss';
 
@@ -13,17 +14,17 @@ import './App.scss';
 //     </div>
 //   );
 // }
-class App extends React.Component {
-  render() {
+function App()  {
     return (
       <div className='App'>
+        <Header />
         <Switch> 
           <Route exact path="/" component={HomePage}/>
           <Route path="/shop" component={ShopPage}/>
         </Switch>  
       </div>
     );
-  }
+
 }
 
 export default App;
